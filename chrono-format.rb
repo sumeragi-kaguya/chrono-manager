3d2ba5bd87bfb8743f20822ba830f2938bbbdd10
Sumeragi Kaguya <nyalice@technologist.com>
2019-08-27 19:43:11 +0300

    Add basic chrono-format script

---
+++ chrono-format.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# chrono-manager: codegeass.ru chronology manager.
+# Copyright (c) 2019 Sumeragi Kaguya <nyalice _at_ technologist.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
+require 'net/http'
+require 'time'
+
+INPUT_FILE = 'tmp.txt'
+
+def read_input_file
+  entries = []
+  entry = []
+
+  File.foreach(INPUT_FILE) do |line|
+    line.chomp!
+
+    if line == '------'
+      entries << entry
+      entry = []
+      next
+    end
+
+    entry << line unless line.empty?
+  end
+
+  pp entries
+end
+
+def main
+  raw_entries = read_input_file
+  pp raw_entries
+end
+
+main if $PROGRAM_NAME == __FILE__
